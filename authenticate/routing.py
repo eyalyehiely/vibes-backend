e7886988773d6e63dcf7291900b3a3d4b2fa45e3
eyalyehiely <eyalwork0@gmail.com>
2024-12-15 13:16:38 +0700

    add chat ws

--- authenticate/routing.py
+++ authenticate/routing.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .consumers import FriendSearchConsumer
+from .consumers import *
 
 websocket_urlpatterns = [
     path('ws/search-friends/', FriendSearchConsumer.as_asgi()),
+    path('ws/chat/', ChatConsumer.as_asgi()),
+
 ]
