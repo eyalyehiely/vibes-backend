1451852d21ccee0d3a5ed53bc2c2dea051540d73
eyalyehiely <eyalwork0@gmail.com>
2024-12-16 17:18:02 +0700

    change allowed hosts

--- vibes/settings.py
+++ vibes/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-#tohv4pmb0sxk*=eb8()9fujgvhc&zv2da&&3%m@#vnpz54=3h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
